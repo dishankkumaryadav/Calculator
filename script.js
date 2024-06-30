5da363732a5298108862522e5b54cdf6a2763f4e
Dishank Kumar Yadav <87142751+dishankkumaryadav@users.noreply.github.com>
2024-06-30 19:08:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+let firstOperand = "";
+let secondOperand = "";
+let currentOperator = null;
+let resetScreen = false;
+
+const display = document.querySelector("#display");
+const numButtons = document.querySelectorAll(".numButton");
+const opButtons = document.querySelectorAll(".opButton");
+const eqButton = document.querySelector(".eqButton");
+const clearButton = document.querySelector(".clearButton");
+
+numButtons.forEach((button) => {
+  button.addEventListener("click", () => appendNumber(button.textContent));
+});
+
+opButtons.forEach((button) => {
+  button.addEventListener("click", () => setOperation(button.textContent));
+});
+
+eqButton.addEventListener("click", () => compute());
+
+clearButton.addEventListener("click", () => clear());
+
+function appendNumber(num) {
+  if (resetScreen) {
+    display.textContent = "";
+    resetScreen = false;
+  }
+  display.textContent += num;
+}
+
+function setOperation(op) {
+  if (currentOperator !== null) compute();
+  firstOperand = display.textContent;
+  currentOperator = op;
+  resetScreen = true;
+}
+
+function compute() {
+  secondOperand = display.textContent;
+  display.textContent = operate(currentOperator, firstOperand, secondOperand);
+  currentOperator = null;
+}
+
+function clear() {
+  display.textContent = "0";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperator = null;
+}
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      if (b === 0) return "Error! Div by 0";
+      return a / b;
+    default:
+      return b;
+  }
+}
